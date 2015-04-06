7263ce627d9a007f23549284d719d89441d9ca8d
calint <calin.tenitchi@gmail.com>
2015-04-05 19:25:31 -0500

    .

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:=$(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE:=libjni
-LOCAL_CFLAGS:=-std=c++11 -Wall -pedantic
+LOCAL_CFLAGS:=-std=c++11 -Wall -Wextra -pedantic
 #-fno-rtti -fno-exceptions
 LOCAL_SRC_FILES:=jni.cpp
 LOCAL_LDLIBS:=-llog -lGLESv2
